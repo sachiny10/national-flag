05bc1a693ea52b784a0212af76c6d367ac6e759e
Sachin <sy945877@gmail.com>
2024-06-16 12:09:21 +0530

    national flag code

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let start  = ()=>{
+
+    setInterval(function(){
+        let a = document.getElementById('orange').classList;
+        a.toggle('whitec')
+    },2000)
+    
+    
+    setInterval(function(){
+        let b = document.getElementById('green').classList;
+        b.toggle('whiteco');
+    },2000)
+}
+
+
